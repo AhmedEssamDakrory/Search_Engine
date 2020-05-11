64eb77a3e6e7cad12e2ba0fb9c19dc615ed4d181
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2020-05-11 02:37:00 +0200

    url seeder file

--- src/Crawler/WebCrawler.java
+++ src/Crawler/WebCrawler.java
@@ -34,7 +34,7 @@ public class WebCrawler {
 	}
 	
 	private void initQueueWithSeededUrls() throws IOException {
-		File file = new File("seeders.txt");   
+		File file = new File("Crawler_seeded_urls.txt");   
 		BufferedReader br = new BufferedReader(new FileReader(file)); 
 		String st; 
 		while ((st = br.readLine()) != null) 
