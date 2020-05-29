34e41334bb51bdac974bc496938a4fe5021a421c
Ayman <ayman.elakwah@gmail.com>
2020-05-29 23:05:02 +0200

    modify ImageSearchResult model

--- src/main/model/ImageSearchResult.java
+++ src/main/model/ImageSearchResult.java
@@ -21,13 +21,6 @@ public class ImageSearchResult extends SearchResult {
         return url;
     }
 
-    public String getBaseUrl() {
-        String[] split = url.split("/");
-        if (split.length >= 3)
-            return split[2];
-        return url;
-    }
-
     public String getTitle() {
         return title;
     }
