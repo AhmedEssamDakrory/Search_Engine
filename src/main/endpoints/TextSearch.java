b857b2ebfbdd9aee2333699e87de3967ca48f5d1
Ayman <ayman.elakwah@gmail.com>
2020-05-30 01:50:55 +0200

    text search endpoint

---
+++ src/main/endpoints/TextSearch.java
@@ -0,0 +1,23 @@
+package main.endpoints;
+
+import com.google.gson.Gson;
+import main.ranker.Ranker;
+import main.utilities.QueryProcessor;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TextSearch extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String query = req.getParameter("query");
+        int page_number = Integer.parseInt(req.getParameter("page_number"));
+        int per_page = Integer.parseInt(req.getParameter("per_page"));
+        QueryProcessor queryProcessor = QueryProcessor.getInstance();
+        String message = new Gson().toJson(Ranker.rank(queryProcessor.process(query), page_number, per_page));
+        resp.setContentType("application/json");
+        resp.getWriter().println(message);
+    }
+}
