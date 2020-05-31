4f126ce36a6d2b99ff8ac0fef8deeefc6fa05f39
Ayman <ayman.elakwah@gmail.com>
2020-05-31 04:31:56 +0200

    ignoring images whose width or height is less than 5 pixels

--- src/main/indexer/IndexingThread.java
+++ src/main/indexer/IndexingThread.java
@@ -13,41 +13,31 @@ import java.nio.file.Paths;
 import java.util.*;
 
 
-public class IndexingThread implements Runnable{
+public class IndexingThread implements Runnable {
     int start, end;
 
-    public IndexingThread(int s, int e){
+    public IndexingThread(int s, int e) {
         start = s;
         end = e;
     }
 
-    public void run() {
-        if (end == -1) end = Indexer.crawledURLs.size();
-        for (int i = start; i<end; i++){
-            processURL(Indexer.crawledURLs.get(i).path, Indexer.crawledURLs.get(i).url);
-        }
-    }
-
     public static String readHtml(String path) {
         String content = "";
-        try
-        {
-            content = new String ( Files.readAllBytes( Paths.get(path) ) );
-        }
-        catch (IOException e)
-        {
+        try {
+            content = new String(Files.readAllBytes(Paths.get(path)));
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return content;
     }
 
-    public static void processURL(String path, String url){
+    public static void processURL(String path, String url) {
         String html = readHtml(Constants.CRAWLED_WEB_PAGES_FILE_PATH + path + ".html");
         org.jsoup.nodes.Document document = Jsoup.parse(html);
         HashMap<String, Integer> wordScores = new HashMap<String, Integer>();
 
         String title = document.title();
-        for (String tagName: Constants.SCORES.keySet()) {
+        for (String tagName : Constants.SCORES.keySet()) {
             Elements tagsText = document.getElementsByTag(tagName);
             Integer score = Constants.SCORES.get(tagName);
             for (Element tagText : tagsText) {
@@ -55,35 +45,36 @@ public class IndexingThread implements Runnable{
             }
         }
         Integer totalScore = 0;
-        for (Integer score: wordScores.values()){
+        for (Integer score : wordScores.values()) {
             totalScore += score;
         }
         ConnectToDB.pushToDatabase(url, title, wordScores, totalScore);
 
         //---------Process Images----------
-        List<Map.Entry<String, Integer> > wordsSorted =
-                new LinkedList<Map.Entry<String, Integer> >(wordScores.entrySet());
+        List<Map.Entry<String, Integer>> wordsSorted =
+                new LinkedList<Map.Entry<String, Integer>>(wordScores.entrySet());
 
         // Sort the list
-        Collections.sort(wordsSorted, new Comparator<Map.Entry<String, Integer> >() {
+        Collections.sort(wordsSorted, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
+                               Map.Entry<String, Integer> o2) {
                 return -(o1.getValue()).compareTo(o2.getValue());
             }
         });
 
         Elements images = document.getElementsByTag("img");
-        for (Element image: images){
+        for (Element image : images) {
             HashMap<String, Integer> captionScore = new HashMap<String, Integer>();
             String src = image.attr("src");
             if (!(src.startsWith("http") || src.startsWith("//")))
-            {
                 continue;
-            }
+            String width = image.attr("width");
+            String height = image.attr("height");
+            if (Integer.parseInt(width != null ? width : "100") <= 5 || Integer.parseInt(height != null ? height : "10") <= 5)
+                continue;
             Integer captionTotalScore = processImage(image, captionScore);
             int num = 0;
-            for (Map.Entry<String, Integer> word: wordsSorted){
+            for (Map.Entry<String, Integer> word : wordsSorted) {
                 Integer prevScore = captionScore.getOrDefault(word, 0);
                 captionScore.put(word.getKey(), prevScore + word.getValue());
                 captionTotalScore += word.getValue();
@@ -94,12 +85,29 @@ public class IndexingThread implements Runnable{
 
     }
 
-    public static void processElement(Element paragraph, Integer score, HashMap<String, Integer> wordScore){
+    public static void processElement(Element paragraph, Integer score, HashMap<String, Integer> wordScore) {
         QueryProcessor q = QueryProcessor.getInstance();
         List<String> words = q.process(paragraph.text());
-        for (String word: words){
+        for (String word : words) {
             Integer prevScore = wordScore.getOrDefault(word, 0);
             wordScore.put(word, prevScore + score);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Map.Entry<String, Integer>> wordsSorted =
+                new LinkedList<Map.Entry<String, Integer>>(Constants.SCORES.entrySet());
+
+        // Sort the list
+        Collections.sort(wordsSorted, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return -(o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        for (Map.Entry<String, Integer> word : wordsSorted) {
+            System.out.println(word.getKey() + " " + word.getValue());
         }
     }
 
@@ -113,21 +121,10 @@ public class IndexingThread implements Runnable{
         return words.size() * Constants.CAPTION_SCORE;
     }
 
-    public static void main(String[] args) {
-        List<Map.Entry<String, Integer> > wordsSorted =
-                new LinkedList<Map.Entry<String, Integer> >(Constants.SCORES.entrySet());
-
-        // Sort the list
-        Collections.sort(wordsSorted, new Comparator<Map.Entry<String, Integer> >() {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
-                return -(o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        for (Map.Entry<String, Integer> word: wordsSorted){
-            System.out.println(word.getKey() + " " + word.getValue());
+    public void run() {
+        if (end == -1) end = Indexer.crawledURLs.size();
+        for (int i = start; i < end; i++) {
+            processURL(Indexer.crawledURLs.get(i).path, Indexer.crawledURLs.get(i).url);
         }
     }
 }
